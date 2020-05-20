d614729aee77252423ae7c8fb9fdf6b43706d426
chddr <spaceshroom175@gmail.com>
2020-05-20 13:18:10 +0300

    Made a silly test

--- src/state.rs
+++ src/state.rs
@@ -1,10 +1,10 @@
 use amethyst::{
-	assets::{AssetStorage, Loader},
-	core::transform::Transform,
-	input::{get_key, is_close_requested, is_key_down, VirtualKeyCode},
-	prelude::*,
-	renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
-	window::ScreenDimensions,
+    assets::{AssetStorage, Loader},
+    core::transform::Transform,
+    input::{get_key, is_close_requested, is_key_down, VirtualKeyCode},
+    prelude::*,
+    renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
+    window::ScreenDimensions,
 };
 
 use log::info;
@@ -12,119 +12,140 @@ use log::info;
 pub struct MyState;
 
 impl SimpleState for MyState {
-	// On start will run when this state is initialized. For more
-	// state lifecycle hooks, see:
-	// https://book.amethyst.rs/stable/concepts/state.html#life-cycle
-	fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
-			let world = data.world;
+    // On start will run when this state is initialized. For more
+    // state lifecycle hooks, see:
+    // https://book.amethyst.rs/stable/concepts/state.html#life-cycle
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let world = data.world;
 
-			// Get the screen dimensions so we can initialize the camera and
-			// place our sprites correctly later. We'll clone this since we'll
-			// pass the world mutably to the following functions.
-			let dimensions = (*world.read_resource::<ScreenDimensions>()).clone();
+        // Get the screen dimensions so we can initialize the camera and
+        // place our sprites correctly later. We'll clone this since we'll
+        // pass the world mutably to the following functions.
+        let dimensions = (*world.read_resource::<ScreenDimensions>()).clone();
 
-			// Place the camera
-			init_camera(world, &dimensions);
+        // Place the camera
+        init_camera(world, &dimensions);
 
-			// Load our sprites and display them
-			let sprites = load_sprites(world);
-			init_sprites(world, &sprites, &dimensions);
-	}
+        // Load our sprites and display them
+        let sprites = load_sprites(world);
+        init_sprites(world, &sprites, &dimensions);
+    }
 
-	fn handle_event(
-			&mut self,
-			mut _data: StateData<'_, GameData<'_, '_>>,
-			event: StateEvent,
-	) -> SimpleTrans {
-			if let StateEvent::Window(event) = &event {
-					// Check if the window should be closed
-					if is_close_requested(&event) || is_key_down(&event, VirtualKeyCode::Escape) {
-							return Trans::Quit;
-					}
+    fn handle_event(
+        &mut self,
+        mut _data: StateData<'_, GameData<'_, '_>>,
+        event: StateEvent,
+    ) -> SimpleTrans {
+        if let StateEvent::Window(event) = &event {
+            // Check if the window should be closed
+            if is_close_requested(&event) || is_key_down(&event, VirtualKeyCode::Escape) {
+                return Trans::Quit;
+            }
 
-					// Listen to any key events
-					if let Some(event) = get_key(&event) {
-							info!("handling key event: {:?}", event);
-					}
+            // Listen to any key events
+            if let Some(event) = get_key(&event) {
+                info!("handling key event: {:?}", event);
+            }
 
-					// If you're looking for a more sophisticated event handling solution,
-					// including key bindings and gamepad support, please have a look at
-					// https://book.amethyst.rs/stable/pong-tutorial/pong-tutorial-03.html#capturing-user-input
-			}
+            // If you're looking for a more sophisticated event handling solution,
+            // including key bindings and gamepad support, please have a look at
+            // https://book.amethyst.rs/stable/pong-tutorial/pong-tutorial-03.html#capturing-user-input
+        }
 
-			// Keep going
-			Trans::None
-	}
+        // Keep going
+        Trans::None
+    }
 }
 
 fn init_camera(world: &mut World, dimensions: &ScreenDimensions) {
-	// Center the camera in the middle of the screen, and let it cover
-	// the entire screen
-	let mut transform = Transform::default();
-	transform.set_translation_xyz(dimensions.width() * 0.5, dimensions.height() * 0.5, 1.);
+    // Center the camera in the middle of the screen, and let it cover
+    // the entire screen
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(dimensions.width() * 0.5, dimensions.height() * 0.5, 1.);
 
-	world
-			.create_entity()
-			.with(Camera::standard_2d(dimensions.width(), dimensions.height()))
-			.with(transform)
-			.build();
+    world
+        .create_entity()
+        .with(Camera::standard_2d(dimensions.width(), dimensions.height()))
+        .with(transform)
+        .build();
 }
 
 fn load_sprites(world: &mut World) -> Vec<SpriteRender> {
-	// Load the texture for our sprites. We'll later need to
-	// add a handle to this texture to our `SpriteRender`s, so
-	// we need to keep a reference to it.
-	let texture_handle = {
-			let loader = world.read_resource::<Loader>();
-			let texture_storage = world.read_resource::<AssetStorage<Texture>>();
-			loader.load(
-					"sprites/logo.png",
-					ImageFormat::default(),
-					(),
-					&texture_storage,
-			)
-	};
+    // Load the texture for our sprites. We'll later need to
+    // add a handle to this texture to our `SpriteRender`s, so
+    // we need to keep a reference to it.
+    let texture_handle = {
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            "sprites/spritesheet.png",
+            ImageFormat::default(),
+            (),
+            &texture_storage,
+        )
+    };
 
-	// Load the spritesheet definition file, which contains metadata on our
-	// spritesheet texture.
-	let sheet_handle = {
-			let loader = world.read_resource::<Loader>();
-			let sheet_storage = world.read_resource::<AssetStorage<SpriteSheet>>();
-			loader.load(
-					"sprites/logo.ron",
-					SpriteSheetFormat(texture_handle),
-					(),
-					&sheet_storage,
-			)
-	};
+    // Load the spritesheet definition file, which contains metadata on our
+    // spritesheet texture.
+    let sheet_handle = {
+        let loader = world.read_resource::<Loader>();
+        let sheet_storage = world.read_resource::<AssetStorage<SpriteSheet>>();
+        loader.load(
+            "sprites/spritesheet.ron",
+            SpriteSheetFormat(texture_handle),
+            (),
+            &sheet_storage,
+        )
+    };
 
-	// Create our sprite renders. Each will have a handle to the texture
-	// that it renders from. The handle is safe to clone, since it just
-	// references the asset.
-	(0..3)
-			.map(|i| SpriteRender {
-					sprite_sheet: sheet_handle.clone(),
-					sprite_number: i,
-			})
-			.collect()
+    // Create our sprite renders. Each will have a handle to the texture
+    // that it renders from. The handle is safe to clone, since it just
+    // references the asset.
+    (0..4)
+        .map(|i| SpriteRender {
+            sprite_sheet: sheet_handle.clone(),
+            sprite_number: i,
+        })
+        .collect()
 }
 
 fn init_sprites(world: &mut World, sprites: &[SpriteRender], dimensions: &ScreenDimensions) {
-	for (i, sprite) in sprites.iter().enumerate() {
-			// Center our sprites around the center of the window
-			let x = (i as f32 - 1.) * 100. + dimensions.width() * 0.5;
-			let y = (i as f32 - 1.) * 100. + dimensions.height() * 0.5;
-			let mut transform = Transform::default();
-			transform.set_translation_xyz(x, y, 0.);
+    let grass = &sprites[0];
+		let stone = &sprites[1];
+		let pers = &sprites[3];
 
-			// Create an entity for each sprite and attach the `SpriteRender` as
-			// well as the transform. If you want to add behaviour to your sprites,
-			// you'll want to add a custom `Component` that will identify them, and a
-			// `System` that will iterate over them. See https://book.amethyst.rs/stable/concepts/system.html
-			world
-					.create_entity()
-					.with(sprite.clone())
-					.with(transform)
-					.build();
-	}
+		let stone_height = 3;
+
+    for x in (0..=dimensions.width() as i32).step_by(64) {
+        for y in 0..stone_height {
+            let mut transform = Transform::default();
+            transform.set_translation_xyz(x as f32, 64. * y as f32, 0.);
+
+            world
+                .create_entity()
+                .with(stone.clone())
+                .with(transform)
+                .build();
+				}
+				
+
+        let mut transform = Transform::default();
+        transform.set_translation_xyz(x as f32, stone_height as f32 * 64., 0.);
+
+        world
+            .create_entity()
+            .with(grass.clone())
+            .with(transform)
+            .build();
+		}
+		
+		
+		let mut transform = Transform::default();
+		transform.set_translation_xyz(288., (stone_height+1) as f32 * 64., 0.);
+
+		world
+				.create_entity()
+				.with(pers.clone())
+				.with(transform)
+				.build();
 }
